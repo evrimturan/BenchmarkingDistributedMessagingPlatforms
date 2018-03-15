49923dbcde3808d22a76745e5dfa9464f7526484
evrimturan <evturan13@ku.edu.tr>
2018-03-15 16:19:14 +0300

    RabbitMQ Producer

---
+++ src/main/java/RabbitMQProducer.java
@@ -0,0 +1,29 @@
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class RabbitMQProducer {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setPort(5672);
+        factory.setHost("159.65.120.184");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+
+}
