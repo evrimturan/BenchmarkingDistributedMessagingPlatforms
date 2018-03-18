cc2d3394aa0f74488fdb93d3a5740a45d8f68a9e
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-18 20:34:21 +0300

    DB connection works ready to generate and insert

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,7 +1,10 @@
+import mysql.connector
 
 
 def _start():
     print('Hello World')
+    _connector = mysql.connector.connect(user='producer', password='producerdb1123@', host='207.154.192.177', database='BILLING_RECORDS', port=23306)
+    _connector.close()
 
 
 if __name__ == '__main__':
