996f11f914503fefc2a634746f53cde89ec6edee
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-26 23:38:22 +0300

    scripts more

--- scripts/rabbit-cookie.sh
+++ scripts/rabbit-cookie.sh
@@ -2,8 +2,8 @@
 
 COOKIE=$( sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-01 "cat /var/lib/rabbitmq/.erlang.cookie" )
 
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-02 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-02 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
