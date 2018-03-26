442803958b2267a1c719cf784dbc2c37d3e91efe
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-26 20:11:55 +0300

    scripts

---
+++ scripts/configure-brokers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo-apt-get update
+sudo apt-get install unzip
+#activemq
+wget http://ftp.itu.edu.tr/Mirror/Apache//activemq/5.15.3/apache-activemq-5.15.3-bin.zip
+unzip -a apache-activemq-5.15.3-bin.zip
+
+#rabbitmq
+echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" | sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list
+sudo apt-get update
+wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_20.3-1~ubuntu~xenial_amd64.deb
+sudo dpkg --install esl-erlang_20.3-1~ubuntu~xenial_amd64.deb
+sudo apt-get -f install
+sudo dpkg --install esl-erlang_20.3-1~ubuntu~xenial_amd64.deb
+wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc |sudo apt-key add -
+sudo apt-get update
+sudo apt-get install rabbitmq-server
+
+#kafka
+wget http://ftp.itu.edu.tr/Mirror/Apache/kafka/1.0.1/kafka_2.11-1.0.1.tgz
+sudo apt-get install openjdk-8-jre
+sudo apt-get install openjdk-8-jdk
+tar xzvf kafka_2.11-1.0.1.tgz
+
+#hosts file
+echo "159.89.102.49 ubuntu-s-1vcpu-1gb-fra1-01" >> /etc/hosts
+echo "159.65.120.184 ubuntu-s-1vcpu-1gb-fra1-02" >> /etc/hosts
+echo "207.154.192.177 ubuntu-s-1vcpu-1gb-fra1-03" >> /etc/hosts
+echo "159.65.119.204 ubuntu-s-1vcpu-1gb-fra1-04" >> /etc/hosts
+echo "159.65.115.195 ubuntu-s-1vcpu-1gb-fra1-05" >> /etc/hosts
+echo "167.99.134.186 ubuntu-s-1vcpu-1gb-fra1-06" >> /etc/hosts
+echo "DONE"
+
---
+++ scripts/stop-kafka-brokers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-01 "cd kafka_2.11-1.0.1/;bin/kafka-server-stop.sh"
+echo "Broker 1 finished"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-02 "cd kafka_2.11-1.0.1/;bin/kafka-server-stop.sh"
+echo "Broker 2 finished"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "cd kafka_2.11-1.0.1/;bin/kafka-server-stop.sh"
+echo "Broker 3 finished"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "cd kafka_2.11-1.0.1/;bin/kafka-server-stop.sh"
+echo "Broker 4 finished"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "cd kafka_2.11-1.0.1/;bin/kafka-server-stop.sh"
+echo "Broker 5 finished"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "cd kafka_2.11-1.0.1/;bin/kafka-server-stop.sh"
+echo "Broker 6 finished"
