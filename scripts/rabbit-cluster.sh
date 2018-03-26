4d04f1f250472d319f84a576a71d3f723fba6be5
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-26 23:31:16 +0300

    scripts more

--- scripts/rabbit-cluster.sh
+++ scripts/rabbit-cluster.sh
@@ -7,18 +7,18 @@ sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-02 "mv /var/lib/rabbitmq/.erlan
 echo "Brokers 1 - 2 clustered"
 sleep 3
 
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-02;rabbitmqctl start_app"
-echo "Brokers 2 - 3 clustered"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-01;rabbitmqctl start_app"
+echo "Brokers 1 - 3 clustered"
 sleep 3
 
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-03;rabbitmqctl start_app"
-echo "Brokers 3 - 4 clustered"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-01;rabbitmqctl start_app"
+echo "Brokers 1 - 4 clustered"
 sleep 3
 
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-04;rabbitmqctl start_app"
-echo "Brokers 4 - 5 clustered"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-01;rabbitmqctl start_app"
+echo "Brokers 1 - 5 clustered"
 sleep 3
 
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-05;rabbitmqctl start_app"
-echo "Brokers 5 - 6 clustered"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "mv /var/lib/rabbitmq/.erlang.cookie /var/lib/rabbitmq/.temp.erlang.cookie;printf "$COOKIE" > /var/lib/rabbitmq/.erlang.cookie;chmod 400 /var/lib/rabbitmq/.erlang.cookie;chown rabbitmq /var/lib/rabbitmq/.erlang.cookie;chgrp rabbitmq /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart;rabbitmqctl stop_app;rabbitmqctl join_cluster rabbit@ubuntu-s-1vcpu-1gb-fra1-01;rabbitmqctl start_app"
+echo "Brokers 1 - 6 clustered"
 sleep 3
---
+++ scripts/rabbit-cookie.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COOKIE=$( sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-01 "cat /var/lib/rabbitmq/.erlang.cookie" )
+
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-02 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
+sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie"
