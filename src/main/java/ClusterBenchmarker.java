690ef9aa53e824cebb57878dfa34cdbd987c4b7d
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-28 23:52:54 +0300

    Init project

---
+++ src/main/java/ClusterBenchmarker.java
@@ -0,0 +1,31 @@
+
+public class ClusterBenchmarker {
+
+    private class Consumer extends Thread{
+
+        @Override
+        public void run(){
+
+        }
+
+        public Consumer(){
+
+        }
+    }
+
+    private class Producer extends Thread{
+
+        @Override
+        public void run(){
+
+        }
+
+        public Producer(){
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
