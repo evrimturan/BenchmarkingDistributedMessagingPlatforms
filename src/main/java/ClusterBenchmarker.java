bfa240a2532a3a42494e5097dd96e25ef1c8baf0
Merve <mbarin14@ku.edu.tr>
2018-03-29 02:16:50 +0300

    ClusterBenchmark_03

--- src/main/java/ClusterBenchmarker.java
+++ src/main/java/ClusterBenchmarker.java
@@ -2,6 +2,7 @@ import org.apache.kafka.common.Cluster;
 
 import javax.security.auth.login.AppConfigurationEntry;
 import javax.security.auth.login.Configuration;
+import javax.xml.crypto.Data;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,7 +16,7 @@ public class ClusterBenchmarker {
 
         @Override
         public void run(){
-
+            System.out.println(Thread.currentThread().getId()+" says hello consumer:)");
         }
 
         public Consumer(){
@@ -27,10 +28,21 @@ public class ClusterBenchmarker {
 
         @Override
         public void run(){
-            System.out.println(Thread.currentThread().getId()+" says hello :)");
+            System.out.println(Thread.currentThread().getId()+" says hello producer:)");
+
         }
 
         public Producer(){
+
+        }
+    }
+    private class DataGenerator extends Thread{
+
+        public void run(){
+            
+        }
+
+        public DataGenerator(){
 
         }
     }
@@ -43,11 +55,13 @@ public class ClusterBenchmarker {
         int pubNum = config.getPubNum();
         int subNum = config.getSubNum();
         double messageSize = config.getMessageSize();
+        double dataSize = config.getDataSize();
         int topicNum = config.getTopicNum();
         List<Producer> pList = new ArrayList<>();
+        List<Consumer> cList = new ArrayList<>();
 
 
-        for(int i=0; i<pubNum;i++){
+        for(int i=0; i<pubNum; i++){
             Producer p = init.createProducer();
             pList.add(p);
             Path path = Paths.get("ProducerFolder"+"-"+i);
@@ -57,9 +71,16 @@ public class ClusterBenchmarker {
                 folder.mkdir();
             }
         }
-
         for(Producer p : pList){
             p.run();
+        }
+
+        for(int i=0; i<subNum; i++){
+            Consumer c = init.createConsumer();
+            cList.add(c);
+        }
+        for(Consumer c : cList){
+            c.run();
         }
         /* TODO: subNum kadar thread daha oluşturulacak
         *  TODO: tüm thread ler run edilecek
@@ -74,10 +95,18 @@ public class ClusterBenchmarker {
                 e.printStackTrace();
             }
         }
+        for(Consumer c : cList){
+            try {
+                c.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         System.out.println("All threads finished.");
     }
 
     private Producer createProducer(){
         return new Producer();
     }
+    private Consumer createConsumer() { return new Consumer(); }
 }
