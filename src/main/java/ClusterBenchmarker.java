adc0f51f01d32046a1e4b303cd7d6e1106a803bd
Merve <mbarin14@ku.edu.tr>
2018-03-29 01:08:20 +0300

    ClusterBenchmark

--- src/main/java/ClusterBenchmarker.java
+++ src/main/java/ClusterBenchmarker.java
@@ -1,3 +1,13 @@
+import org.apache.kafka.common.Cluster;
+
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClusterBenchmarker {
 
@@ -27,5 +37,29 @@ public class ClusterBenchmarker {
 
     public static void main(String[] args) {
 
+        ClusterBenchmarker init = new ClusterBenchmarker();
+        TestConfiguration config = new TestConfiguration(args[1]);
+        int brokerNum = config.getBrokerNum();
+        int pubNum = config.getPubNum();
+        int subNum = config.getSubNum();
+        double messageSize = config.getMessageSize();
+        int topicNum = config.getTopicNum();
+        List<Producer> pList = new ArrayList<Producer>();
+
+
+        for(int i=0; i<pubNum;i++){
+            Producer p = init.createProducer();
+            pList.add(p);
+            Path path = Paths.get("ProducerFolder");
+
+            if (!Files.exists(path)) {
+                File folder = new File("ProducerFolder");
+            }
+        }
+
+    }
+
+    private Producer createProducer(){
+        return new Producer();
     }
 }
