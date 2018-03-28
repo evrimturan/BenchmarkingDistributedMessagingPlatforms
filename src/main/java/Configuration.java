f5ecd70da522921b88536c152c0440c12291f83d
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-03-28 23:58:16 +0300

    Config

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,4 +1,61 @@
 
 public class Configuration {
+    private int brokerNum;
+    private int pubNum;
+    private int subNum;
+    private double messageSize;
+    private int topicNum;
+    private boolean persistent;
 
+    public Configuration(String Filename){
+        
+    }
+
+    public int getBrokerNum() {
+        return brokerNum;
+    }
+
+    public void setBrokerNum(int brokerNum) {
+        this.brokerNum = brokerNum;
+    }
+
+    public int getPubNum() {
+        return pubNum;
+    }
+
+    public void setPubNum(int pubNum) {
+        this.pubNum = pubNum;
+    }
+
+    public int getSubNum() {
+        return subNum;
+    }
+
+    public void setSubNum(int subNum) {
+        this.subNum = subNum;
+    }
+
+    public double getMessageSize() {
+        return messageSize;
+    }
+
+    public void setMessageSize(double messageSize) {
+        this.messageSize = messageSize;
+    }
+
+    public int getTopicNum() {
+        return topicNum;
+    }
+
+    public void setTopicNum(int topicNum) {
+        this.topicNum = topicNum;
+    }
+
+    public boolean isPersistent() {
+        return persistent;
+    }
+
+    public void setPersistent(boolean persistent) {
+        this.persistent = persistent;
+    }
 }
