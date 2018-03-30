d809185e3e173a7ad71c10010c7e0c3075c2e66e
evrimturan <evturan13@ku.edu.tr>
2018-03-31 02:06:42 +0300

    sh kafka for

--- scripts/start-kafka-brokers.sh
+++ scripts/start-kafka-brokers.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+hostName="root@ubuntu-s-1vcpu-1gb-fra1-0"
 
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-02 "cd kafka_2.11-1.0.1/;bin/zookeeper-server-start.sh -daemon config/zookeeper.properties;bin/kafka-server-start.sh -daemon config/server.properties"
-echo "Broker 2 finished"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-01 "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
-echo "Broker 1 finished"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
-echo "Broker 3 finished"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
-echo "Broker 4 finished"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
-echo "Broker 5 finished"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
-echo "Broker 6 finished"
+for brokerNumber in $(eval echo "{1..$2}")
+do
+    if [ $brokerNumber -eq 1 ];
+    then
+        echo "Broker 1 Zookeeper Kafka"
+        sshpass -p "$1" ssh "$hostName$brokerNumber" "cd kafka_2.11-1.0.1/;bin/zookeeper-server-start.sh -daemon config/zookeeper.properties;bin/kafka-server-start.sh -daemon config/server.properties"
+    else
+        echo "Brokers Kafka"
+        sshpass -p "$1" ssh "$hostName$brokerNumber" "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
+    fi
+    echo "Broker $brokerNumber Finished"
+done
