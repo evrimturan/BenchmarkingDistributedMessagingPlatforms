627a14e77be71845bb84ae504869c37df3018025
Merve Barın <mbarin14@ku.edu.tr>
2018-04-03 22:50:54 +0300

    CLusterBenchmark_05

--- src/main/java/ClusterBenchmarker.java
+++ src/main/java/ClusterBenchmarker.java
@@ -7,10 +7,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.Arrays;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.ActiveMQSession;
 import org.apache.activemq.command.ActiveMQBytesMessage;
+import com.rabbitmq.client.*;
+//import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import javax.jms.*;
 
@@ -22,6 +32,7 @@ public class ClusterBenchmarker {
         private String platform;
         private long totalTimeElapsed;
         private int queueNum;
+        private final static String QUEUE_NAME = "hello";
 
         public long getTotalTimeElapsed() {
             return totalTimeElapsed;
@@ -34,7 +45,7 @@ public class ClusterBenchmarker {
                 ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://ubuntu-s-1vcpu-1gb-fra1-01:61616");
                 try{
                     FileOutputStream fos = new FileOutputStream(folderName+"/consumer.data-"+queueNum);
-                    Connection connection = connectionFactory.createConnection("admin","admin");
+                    javax.jms.Connection connection = connectionFactory.createConnection("admin","admin");
                     connection.start();
         
                     // Create a Session
@@ -43,13 +54,13 @@ public class ClusterBenchmarker {
                     Queue dest = session.createQueue("queue-"+queueNum);
     
                     MessageConsumer consume = session.createConsumer(dest);
-                    System.out.println("LO LO LO");
+                    //System.out.println("LO LO LO");
                     long start = System.currentTimeMillis();
                     ActiveMQBytesMessage rc =  (ActiveMQBytesMessage)consume.receive(100);
                     long end = System.currentTimeMillis();
                     totalTimeElapsed = end-start;
                     System.out.println("Consumed in "+ totalTimeElapsed +" ms");
-                    System.out.println("LO LO LO bitti");
+                    //System.out.println("LO LO LO bitti");
                     byte[] buffer = new byte[81920];
                     
                     while((rc.readBytes(buffer)) != -1){
@@ -61,6 +72,53 @@ public class ClusterBenchmarker {
 
                 }catch(Exception ex){
                     ex.printStackTrace();
+                }
+            }else if (platform.equals("rabbitmq")){
+                try{
+                    com.rabbitmq.client.ConnectionFactory factory = new com.rabbitmq.client.ConnectionFactory();
+                    factory.setUsername("admin");
+                    factory.setPassword("admin");
+                    factory.setPort(5672);
+                    factory.setHost("159.65.120.184");
+                    com.rabbitmq.client.Connection connection = factory.newConnection();
+                    Channel channel = connection.createChannel();
+
+                    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+                    com.rabbitmq.client.Consumer consumer = new DefaultConsumer(channel) {
+                    @Override
+                    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                        throws IOException {
+                            String message = new String(body, "UTF-8");
+                            System.out.println(" [x] Received '" + message + "'");
+                        }
+                    };
+                    channel.basicConsume(QUEUE_NAME, true, consumer);   
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }else if (platform.equals("kafka")){
+                Properties props = new Properties();
+                props.put("bootstrap.servers", "159.65.120.184:9092");
+                //props.put("bootstrap.servers", "159.89.102.49:9092");
+                props.put("group.id", "group-1");
+                props.put("enable.auto.commit", "true");
+                props.put("auto.commit.interval.ms", "1000");
+                props.put("auto.offset.reset", "earliest");
+                props.put("session.timeout.ms", "30000");
+                props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+                props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+                org.apache.kafka.clients.consumer.KafkaConsumer<String, String> consumer = null;
+                try {
+                    consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<String, String>(props);
+                    consumer.subscribe(Arrays.asList("failsafe"));
+                    ConsumerRecords<String, String> records = consumer.poll(100);
+                    for (ConsumerRecord<String, String> record : records) {
+                        System.out.println("Received = " + record.value());
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
             //long finish = System.currentTimeMillis();
@@ -82,6 +140,7 @@ public class ClusterBenchmarker {
         private long totalTimeEllapsed;
         private int queueNum;
         private String folderName;
+        private final static String QUEUE_NAME = "hello";
 
         public long getTotalTimeEllapsed() {
             return totalTimeEllapsed;
@@ -96,7 +155,7 @@ public class ClusterBenchmarker {
                 connectionFactory.setProducerWindowSize((int)dSize);
                 try{
                     FileInputStream in = new FileInputStream(new File(folderName+"/producer.data-"+queueNum));
-                    Connection connection = connectionFactory.createConnection("admin","admin");
+                    javax.jms.Connection connection = connectionFactory.createConnection("admin","admin");
                     connection.start();
 
                     // Create a Session
@@ -121,6 +180,57 @@ public class ClusterBenchmarker {
                 }catch(Exception ex){
                     ex.printStackTrace();
                 }
+            }else if (platform.equals("rabbitmq")){
+                try{
+                    com.rabbitmq.client.ConnectionFactory factory = new com.rabbitmq.client.ConnectionFactory();
+                    factory.setUsername("admin");
+                    factory.setPassword("admin");
+                    factory.setPort(5672);
+                    factory.setHost("159.89.102.49");
+                    com.rabbitmq.client.Connection connection = factory.newConnection();
+                    Channel channel = connection.createChannel();
+
+                    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                    String message = "Hello World!";
+                    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+                    System.out.println(" [x] Sent '" + message + "'");
+
+                    channel.close();
+                    connection.close();
+                }catch(Exception e){
+
+                }
+            }else if(platform.equals("kafka")){
+                String topicName = "failsafe";
+                Properties props = new Properties();
+                props.put("bootstrap.servers", "159.89.102.49:9092");
+                //props.put("bootstrap.servers", "159.65.120.184:9092");
+                props.put("acks", "all");
+                props.put("retries", 0);
+                props.put("batch.size", 16384);
+                props.put("linger.ms", 1);
+                props.put("buffer.memory", 33554432);
+
+                props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+                props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+                org.apache.kafka.clients.producer.Producer<String, String> producer = null;
+                try {
+                    producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(props);
+
+                    for(int i = 0; i < 10; i++) {
+                        producer.send(new ProducerRecord<String, String>(topicName, "Message " + Integer.toString(i + 100)));
+                        System.out.println("Message sent successfully");
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                finally {
+                    producer.close();
+                }
+
             }
             
             //long finish = System.currentTimeMillis();
