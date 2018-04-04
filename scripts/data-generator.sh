2cf3a8d8d693bb3ea954ab53e40f51904dfe499c
evrimturan <evturan13@ku.edu.tr>
2018-04-04 23:40:10 +0300

    File

--- scripts/data-generator.sh
+++ scripts/data-generator.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-filename="producer.data-"
-
-for index in $(eval echo "{0..$1}")
-do
-    openssl rand -out $3/$filename$index -base64 $(( $2 * 3/4 ))
-done
-
+openssl rand -out "$3"/"producer.data-1KB" -base64 $(( 2**10 * 3/4 ))
+openssl rand -out "$3"/"producer.data-128KB" -base64 $(( 2**17 * 3/4 ))
+openssl rand -out "$3"/"producer.data-256KB" -base64 $(( 2**18 * 3/4 ))
+openssl rand -out "$3"/"producer.data-512KB" -base64 $(( 2**19 * 3/4 ))
+openssl rand -out "$3"/"producer.data-1MB" -base64 $(( 2**20 * 3/4 ))
+openssl rand -out "$3"/"producer.data-128MB" -base64 $(( 2**27 * 3/4 ))
+openssl rand -out "$3"/"producer.data-512MB" -base64 $(( 2**29 * 3/4 ))
+openssl rand -out "$3"/"producer.data-1GB" -base64 $(( 2**30 * 3/4 ))
 
 
 
