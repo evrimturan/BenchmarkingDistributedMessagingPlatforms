2822e678ff4a0e201c90bb28a582d01717533e4b
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-05-01 02:59:08 +0300

    fix

--- scripts/start-kafka-brokers.sh
+++ scripts/start-kafka-brokers.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-hostName="root@ubuntu-s-1vcpu-1gb-fra1-0"
+hostName="mustafa@ubuntu-s-1vcpu-1gb-fra1-0"
 
-for brokerNumber in $(eval echo "{1..$2}")
+for brokerNumber in $(eval echo "{1..$1}")
 do
     if [ $brokerNumber -eq 1 ];
     then
         echo "Broker 1 Zookeeper Kafka"
-        sshpass -p "$1" ssh "$hostName$brokerNumber" "cd kafka_2.11-1.0.1/;bin/zookeeper-server-start.sh -daemon config/zookeeper.properties;bin/kafka-server-start.sh -daemon config/server.properties"
+        ssh "$hostName$brokerNumber" "cd kafka_2.11-1.0.1/;bin/zookeeper-server-start.sh -daemon config/zookeeper.properties;bin/kafka-server-start.sh -daemon config/server.properties"
     else
         echo "Brokers Kafka"
-        sshpass -p "$1" ssh "$hostName$brokerNumber" "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
+        ssh "$hostName$brokerNumber" "cd kafka_2.11-1.0.1/;bin/kafka-server-start.sh -daemon config/server.properties"
     fi
     echo "Broker $brokerNumber Finished"
     sleep 1
