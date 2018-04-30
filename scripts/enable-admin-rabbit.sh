ecdb674dedc5c43c0e1d5e6327d65dbc40bc98c0
Mustafa Acıkaraoğlu <macikaraoglu13@ku.edu.tr>
2018-04-30 21:35:12 +0300

    cons prod fix multi queue consume produce

--- scripts/enable-admin-rabbit.sh
+++ scripts/enable-admin-rabbit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-hostName="root@ubuntu-s-1vcpu-1gb-fra1-0"
+hostName="mustafa@ubuntu-s-1vcpu-1gb-fra1-0"
 
-for brokerNumber in $(eval echo "{1..$2}")
+for brokerNumber in $(eval echo "{1..$1}")
 do
-    sshpass -p "$1" ssh "$hostName$brokerNumber" "rabbitmq-plugins enable rabbitmq_management"
+    ssh "$hostName$brokerNumber" "rabbitmq-plugins enable rabbitmq_management"
     echo "Broker $brokerNumber Finished"
     sleep 1
 done
--- scripts/rabbit-cookie.sh
+++ scripts/rabbit-cookie.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-COOKIE=$( sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-01 "cat /var/lib/rabbitmq/.erlang.cookie" )
+COOKIE=$( ssh root@ubuntu-s-1vcpu-1gb-fra1-01 "cat /var/lib/rabbitmq/.erlang.cookie" )
 echo "$COOKIE"
 
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-02 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-03 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-04 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
-sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+ssh mustafa@ubuntu-s-1vcpu-1gb-fra1-02 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+ssh mustafa@ubuntu-s-1vcpu-1gb-fra1-03 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+ssh mustafa@ubuntu-s-1vcpu-1gb-fra1-04 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+#sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-05 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
+#sshpass -p "$1" ssh root@ubuntu-s-1vcpu-1gb-fra1-06 "printf $COOKIE > /var/lib/rabbitmq/.erlang.cookie;service rabbitmq-server restart"
